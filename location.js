88c1f1f3bf2d8cac4df858ec7476921caf0af699
Archerymidnight <kiernandougie@gmail.com>
2022-12-15 20:49:46 +0000

    Added event listener, local storage for vars

--- location.js
+++ location.js
@@ -1,21 +1,25 @@
-let fromLoc = 'dd15dl';
-let toLoc = document.getElementById('TP').value;
 
-let fromUrl = 'https://api.postcodes.io/postcodes/' + fromLoc;
-let toUrl = 'https://api.postcodes.io/postcodes/' + toLoc;
+$('.btn').click(getCoords);
 
-fetch(fromUrl)
+function getCoords(){
+
+  fromLoc = document.getElementById('FP').value;
+  toLoc = document.getElementById('TP').value;
+
+  fromUrl = 'https://api.postcodes.io/postcodes/' + fromLoc;
+  toUrl = 'https://api.postcodes.io/postcodes/' + toLoc;
+
+  fetch(fromUrl)
     .then((resp) => resp.json())
-    .then(function(data) {
-      
+    .then(function (data) {
+
       lat = data.result.latitude;
-      long = data.result.longitude
-      console.log(lat);
-      console.log(long);` `
-
-
+      long = data.result.longitude;
+      localStorage.setItem('latitude', lat);
+      localStorage.setItem('longitude', long);
 
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log(error);
     });
+}
